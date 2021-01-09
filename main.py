096a861a13d540d749d83e77d28ae4649dabe0da
iBoy <547142436@qq.com>
2021-01-09 21:17:08 +0800

    初始化脚本

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ qq = os.environ['QQ']
 
 def main(*args):
     api = "https://api.xmdpay.cn/API/mpz/api.php?qq="+qq
-    for i in range(0, 21):
-        data = requests.get(api, timeout=600)
-        if "success" in data.text:
-            print("第 {} 次刷赞成功".format(i+1))
-            time.sleep(10)
+    # for i in range(0, 21):
+    data = requests.get(api, timeout=600)
+    if "success" in data.text:
+        print("刷赞成功")
+        # print("第 {} 次刷赞成功".format(i+1))
+        # time.sleep(10)
 
 
 if __name__ == '__main__':
